ac9728693c24e73909f359a822d509486788172b
Jeff Kungu Gitenguri <jgitenguri@safaricom.co.ke>
2019-04-15 19:46:54 +0300

    [Feature] integrates open weather api

--- Forecast/app/src/main/java/com/forecast/forecast/Contract.java
+++ Forecast/app/src/main/java/com/forecast/forecast/Contract.java
@@ -1,5 +1,7 @@
 package com.forecast.forecast;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by jeffkungu on 15/04/2019.
  */
@@ -9,8 +11,14 @@ public interface Contract {
         void displayGoogleServiceError(int serviceAvailable);
         void displayMessage(String message);
         void initialiseViews();
+        void moveCameraView(LatLng latLng, float zoom, String locationName);
+
+        void displayWetherDetails(String locationName, String skyConditions, String averageTemp,
+                                  String pressure, String humidity, String windSpeed);
     }
     interface MainViewEventlistener {
         void checkGoogleServiceAvailability();
+        void findLocation(String locationName);
+        void getWetherDetails(String locationName);
     }
 }
--- Forecast/app/src/main/java/com/forecast/forecast/MainActivity.java
+++ Forecast/app/src/main/java/com/forecast/forecast/MainActivity.java
@@ -3,6 +3,9 @@ package com.forecast.forecast;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
@@ -12,28 +15,50 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.forecast.forecast.presenter.MainPresenter;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity implements Contract.MainView, View.OnClickListener, OnMapReadyCallback {
-    private ConstraintLayout mainConstraint;
-    private FloatingActionButton fab1;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Contract.MainView, View.OnClickListener, OnMapReadyCallback, TextView.OnEditorActionListener {
+    private ConstraintLayout constraintWeatherInfo;
+    private FloatingActionButton fab1, fab2;
+    private ImageView getWeatherImage;
     private Contract.MainViewEventlistener eventlistener;
     private boolean permissionGranted = false;
     private GoogleMap map;
+    private FusedLocationProviderClient locationProviderClient;
+    private AutoCompleteTextView autoCompleteSearch;
+    private MarkerOptions markerOptions;
+    private TextView txtWeatherTitle, txtSkyConditionValue, txtPressureValue, txtHumidityValue, txtWindSpeedValue,
+            txtSave, txtCancel, txtTemperatureValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbarMain);
-        setSupportActionBar(toolbar);
-        mainConstraint = findViewById(R.id.mainConstraint);
+
         eventlistener = new MainPresenter(this, this);
         eventlistener.checkGoogleServiceAvailability();
 
@@ -43,18 +68,63 @@ public class MainActivity extends AppCompatActivity implements Contract.MainView
     @Override
     public void initialiseViews() {
         fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
+        getWeatherImage = findViewById(R.id.getWeatherImage);
+        autoCompleteSearch = findViewById(R.id.autoCompleteSearch);
+        constraintWeatherInfo = findViewById(R.id.constraintWeatherInfo);
+        txtSkyConditionValue = findViewById(R.id.txtSkyConditionValue);
+        txtPressureValue = findViewById(R.id.txtPressureValue);
+        txtHumidityValue = findViewById(R.id.txtHumidityValue);
+        txtWindSpeedValue = findViewById(R.id.txtWindSpeedValue);
+        txtTemperatureValue = findViewById(R.id.txtTemperatureValue);
+        txtWeatherTitle = findViewById(R.id.txtWeatherTitle);
+        txtSave = findViewById(R.id.txtSave);
+        txtCancel = findViewById(R.id.txtCancel);
 
         fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+        getWeatherImage.setOnClickListener(this);
+        txtSave.setOnClickListener(this);
+        txtCancel.setOnClickListener(this);
+        autoCompleteSearch.setOnEditorActionListener(this);
+    }
+
+    private void getUserLocation() {
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            if (permissionGranted) {
+                Task task = locationProviderClient.getLastLocation();
+                task.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            Location userLocation = (Location) task.getResult();
+                            if (userLocation != null){
+                                LatLng latLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
+                                moveCameraView(latLng, 20f, "Current Location");
+                            }
+                        } else {
+                            displayMessage(getString(R.string.find_location_error));
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+
+        }
     }
 
     private void checkPermissions() {
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION};
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                     Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 permissionGranted = true;
+                initialiseMap();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1);
             }
         } else {
             ActivityCompat.requestPermissions(this, permissions, 1);
@@ -68,11 +138,11 @@ public class MainActivity extends AppCompatActivity implements Contract.MainView
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case 1:
-                if(grantResults.length > 0) {
-                    for(int i = 0 ; i < grantResults.length ; i++) {
-                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             permissionGranted = false;
                             return;
                         }
@@ -95,13 +165,26 @@ public class MainActivity extends AppCompatActivity implements Contract.MainView
 
     @Override
     public void displayMessage(String message) {
-        Snackbar.make(mainConstraint, message, Snackbar.LENGTH_LONG).show();
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.fab1:
+                displayMessage("Floating button clicked");
+                break;
+
+            case R.id.fab2:
+                displayMessage("Floating button clicked");
+                break;
+
+            case R.id.getWeatherImage:
+                eventlistener.getWetherDetails(autoCompleteSearch.getText().toString());
+                break;
+
+            case R.id.txtCancel:
+                constraintWeatherInfo.setVisibility(View.INVISIBLE);
                 break;
 
             default:
@@ -112,5 +195,49 @@ public class MainActivity extends AppCompatActivity implements Contract.MainView
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        if (permissionGranted) {
+            getUserLocation();
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+    @Override
+    public void moveCameraView(LatLng latLng, float zoom, String locationName) {
+        map.clear();
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        markerOptions = new MarkerOptions()
+                .position(latLng)
+                .title(locationName);
+        map.addMarker(markerOptions);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH
+                || actionId == EditorInfo.IME_ACTION_DONE){
+            String locationName = autoCompleteSearch.getText().toString();
+            eventlistener.findLocation(locationName);
+        }
+        return false;
+    }
+
+    @Override
+    public void displayWetherDetails(String locationName, String skyConditions, String averageTemp,
+                                     String pressure, String humidity, String windSpeed){
+        constraintWeatherInfo.setVisibility(View.VISIBLE);
+        txtWeatherTitle.setText(locationName +  " " +getResources().getString(R.string.weather_condition));
+        txtSkyConditionValue.setText(skyConditions);
+        txtTemperatureValue.setText(averageTemp);
+        txtPressureValue.setText(pressure);
+        txtHumidityValue.setText(humidity);
+        txtWindSpeedValue.setText(windSpeed);
     }
 }
